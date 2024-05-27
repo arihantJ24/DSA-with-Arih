9b0ce434be8fa2c8ed180ea532c6769622d923f3
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-05-27 20:51:24 +0530

    Time: 56 ms (66.02%) | Memory: 7.8 MB (98.23%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -5,17 +5,34 @@ public:
         reverse(t.begin(),t.end());
         int n = s.length();
         
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // for(int i = 1;i<=n;i++){
+        //     for(int j= 1;j<=n;j++){
+        //         if(s[i-1]== t[j-1]){
+        //             dp[i][j] = 1+dp[i-1][j-1];
+        //         }
+        //         else{
+        //             dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+        //         }
+        //     }
+        // }
+        // return dp[n][n];  
+
+        // Optimised
+        vector<int>prev(n+1,0),curr(n+1,0);
         for(int i = 1;i<=n;i++){
             for(int j= 1;j<=n;j++){
                 if(s[i-1]== t[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    curr[j] = 1+prev[j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+                   curr[j] = max(curr[j-1],prev[j]);
                 }
             }
+            prev = curr;
         }
-        return dp[n][n];  
+        return prev[n];  
     }
+
+
 };
