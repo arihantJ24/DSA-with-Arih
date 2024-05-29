1b9064009a3ccd124e8b360b2e43d027ee5f56fa
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-05-29 15:45:02 +0530

    Time: 15 ms (74.08%) | Memory: 31.4 MB (23.87%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    //memoization
-    int f1(string &s, string &t , int i , int j ,vector<vector<int>>&dp){
+    // ------------------------- Memoization -------------------
+    /*int f1(string &s, string &t , int i , int j ,vector<vector<int>>&dp){
         if(j==0)return 1;
         if(i==0) return 0;
 
@@ -9,13 +9,30 @@ public:
 
         if(s[i-1] == t[j-1]) return dp[i][j] = f1(s,t,i-1,j-1,dp) + f1(s,t,i-1,j,dp);
         return dp[i][j] = f1(s,t,i-1,j,dp);
-        
-    }
+
+    } */
+
+    
+
+    
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f1(s,t,n,m,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // return f1(s,t,n,m,dp);
+
+        // ---------------------- Tabulation -----------------------
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        for(int i =0 ;i<=n;i++){
+            dp[i][0] =1;
+        }
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+                if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
     }
 };
