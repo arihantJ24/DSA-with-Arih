2215a07891e65f4ebe3a86f67e0bb15b13e26e8e
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-05-30 15:44:10 +0530

    Time: 3 ms (92.52%) | Memory: 12 MB (33.69%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
-    int f(string & word1, string & word2 , int i , int j ,vector<vector<int>>&dp){
-        if(i<0){
-            return j+1; // word1 is exhausted so we print the count of string 2 that is left
+    /* int f(string & word1, string & word2 , int i , int j ,vector<vector<int>>&dp){
+        if(i == 0){
+            return j; // word1 is exhausted so we print the count of string 2 that is left
         }
-        if(j<0){
-            return i+1; // word1 is exhausted so we print the count of string 2 that is left
+        if(j == 0){
+            return i; // word2 is exhausted so we print the count of string 1 that is left
         }
         if(dp[i][j]!= -1)return dp[i][j];
 
-        if(word1[i]== word2[j]) return dp[i][j] = 0+ f(word1,word2, i-1 , j-1,dp);
+        if(word1[i-1]== word2[j-1]) return dp[i][j] = 0+ f(word1,word2, i-1 , j-1,dp);
         return dp[i][j] = 1+ min({ f(word1,word2, i-1 , j-1,dp),f(word1,word2, i , j-1,dp),f(word1,word2, i-1 , j,dp)});
 
-    }
+    } */
     int minDistance(string word1, string word2) {
         int n = word1.length() , m = word2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(word1, word2, n-1, m-1, dp);
+        
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // return f(word1, word2, n, m, dp);
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i =0 ;i<=n;i++){
+            dp[i][0] = i;
+        }
+
+        for(int j = 0 ;j<=m;j++){
+            dp[0][j] = j;
+        }
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(word1[i-1]== word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1+ min({ dp[i-1][j-1] , dp[i][j-1], dp[i-1][j]});
+                }
+            }
+        }
+        return dp[n][m];
+        
+
     }
 };
