210856f15f1704c604fa6395c0b945c8463abdb4
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-06-02 23:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (50.67%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* --------------------------------- Memoization -----------------------
     int f(int idx, int buy, int n,vector<int>& prices,vector<vector<int>>&dp ){
         if(idx >= n)return 0;// instead of idx == n we use idx>= n if this happen we return 0 as after selling we have a coodwom period of one day so we go to (idx+2)index;
         if(dp[idx][buy]!=-1)return dp[idx][buy];
@@ -13,9 +14,34 @@ public:
       
         }  
     }
+    */
+
+    int f2(vector<int>&prices){
+        int n = prices.size();
+        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int idx = n-1;idx>=0;idx--){
+            for(int buy = 0;buy<=1;buy++){             
+                if(buy ==0){
+                    dp[idx][buy] = max(0+dp[idx+1][0], -prices[idx]+dp[idx+1][1] );
+                }
+                else{
+                    dp[idx][buy] = max(0+dp[idx+1][1], prices[idx]+dp[idx+2][0] );
+                }            
+            }
+        }
+        return dp[0][0];
+    }
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
+        /* --------------------------------- Memoization -----------------------
         vector<vector<int>>dp(n,vector<int>(2,-1));
         return f(0,0,n,prices,dp);
+        */
+        // --------------------------------- Tabulation -----------------------
+        return f2(prices);
+        
     }
 };
