ead4bbd2086700348a7d4ed2b72e8fd9519cbe2b
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-06-19 14:51:35 +0530

    Time: 13 ms (8.11%) | Memory: 17 MB (40.43%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-void dfs(int row ,int col , int initialColor , int color,vector<vector<int>>& image ,vector<vector<int>>& newMat ){
-    newMat[row][col] = color;
-    int drow[4] = {-1,0,1,0};
-    int dcol[4] = {0,1,0,-1};
 
-    for(int i = 0 ;i<4;i++){
-        int nrow = row + drow[i];
-        int ncol = col + dcol[i];
-
-        if(nrow >= 0 && nrow < image.size() && ncol >= 0 && ncol < image[0].size() &&
-        image[nrow][ncol] == initialColor && newMat [nrow][ncol] != color ){
-            dfs(nrow, ncol , initialColor , color ,image, newMat);
-        }
-    }
-}
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>> newMat = image;
-        int n = image.size();
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,int color) {
+        if (image[sr][sc] == color) return image;
         int initialColor = image[sr][sc];
-        dfs(sr,sc,initialColor,color,image,newMat);
-        return newMat;
+        int n = image.size();
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;
+        int drow[4] = {-1, 0, 1, 0};
+        int dcol[4] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                for (int i = 0; i < 4; i++) {
+                    int nrow = row + drow[i];
+                    int ncol = col + dcol[i];
+                    if (nrow >= 0 && nrow < image.size() && ncol >= 0 &&  ncol < image[0].size() &&
+                        image[nrow][ncol] == initialColor) {
+                        image[nrow][ncol] = color;
+                        q.push({nrow,ncol});
+                    }
+                }
+            }
+        }
+
+        return image;
     }
 };
