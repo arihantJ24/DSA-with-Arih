b1c5c7960ba025a43c74a44f10459332cc98b977
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-07-08 14:55:50 +0530

    Time: 31 ms (9.75%) | Memory: 25.7 MB (20.59%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int helper(ListNode*head, int k){
         ListNode*temp = head;
         while(head->next != head){
@@ -14,21 +15,40 @@ public:
             temp = prev->next;
         }
         return head->val;
-    }
+    } */
+
     int findTheWinner(int n, int k) {
         // ------------------------------M1 -> circular list---------------------
-        if(k==1)return n;
-        ListNode*head = NULL, *tail = NULL;
-        for(int i = 1;i<=n;i++){
-            ListNode* node = new ListNode(i);
-            if(head== NULL) head= tail = node;
-            else{
-                tail->next = node;
-                tail = node;
-            }
-            tail->next= head; // for circular list
-        }
-        return helper(head,k);
+        // if(k==1)return n;
+        // ListNode*head = NULL, *tail = NULL;
+        // for(int i = 1;i<=n;i++){
+        //     ListNode* node = new ListNode(i);
+        //     if(head== NULL) head= tail = node;
+        //     else{
+        //         tail->next = node;
+        //         tail = node;
+        //     }
+        //     tail->next= head; // for circular list
+        // }
+        // return helper(head,k);
 
+
+        // ------------------------------M2 -> Queue---------------------
+
+        queue<int>q;
+        for(int i =1;i<=n;i++){
+            q.push(i);
+        }
+
+        while(q.size() != 1){
+            int size = k;
+            for(int i=1;i<size;i++){
+                int val = q.front();
+                q.pop();
+                q.push(val);
+            }
+            q.pop();
+        }
+        return q.front();
     }
 };
