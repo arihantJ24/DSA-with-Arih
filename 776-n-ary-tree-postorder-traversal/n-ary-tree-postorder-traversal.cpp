3116da5bf65724235f21f9efde46821afef82701
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-08-26 18:50:27 +0530

    Time: 18 ms (22.62%) | Memory: 15.2 MB (70.21%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void traverse(Node* root , vector<int>&post){
+        if(!root)return ;
+        for(auto x : root->children){
+            traverse(x,post);
+
+        }
+        post.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+  
+        vector<int>post;
+        traverse(root, post);
+        return post;
+        
+    }
+};
